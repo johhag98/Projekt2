a0c81fad02e4b6c418a2e68ca41246b3c964b285
Johan Hagströmer <johan.hagstromer@gmail.com>
2015-11-27 13:45:20 +0100

    Bug fixes

--- scr/scr/Form1.cs
+++ scr/scr/Form1.cs
@@ -26,7 +26,7 @@ namespace scr
 
             string andText2= tbxAnd2.Text;
             int and2;
-            bool andTest21 = int.TryParse(andText1, out and2);
+            bool andTest21 = int.TryParse(andText2, out and2);
             bool andTest22 = and2 == 1 || and2 == 0;
 
 
@@ -35,7 +35,7 @@ namespace scr
                 MessageBox.Show("Du måste skriva in antingen en etta eller nolla i båda rutorna, tack!", "Viktig information", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-            if (and1 == 1 && and2 == 1)
+            if ((and1 == 1) && (and2 == 1))
             {
                 lblSvarAnd.Text = "1";
                 lblSvarAnd.ForeColor = Color.Green;
@@ -50,14 +50,14 @@ namespace scr
 
         private void pbxNand_Click(object sender, EventArgs e)
         {
-            string andText1 = tbxAnd1.Text;
+            string andText1 = tbxNand1.Text;
             int and1;
             bool andTest11 = int.TryParse(andText1, out and1);
             bool andTest12 = and1 == 1 || and1 == 0;
 
-            string andText2= tbxAnd2.Text;
+            string andText2= tbxNand2.Text;
             int and2;
-            bool andTest21 = int.TryParse(andText1, out and2);
+            bool andTest21 = int.TryParse(andText2, out and2);
             bool andTest22 = and2 == 1 || and2 == 0;
 
 
@@ -68,13 +68,13 @@ namespace scr
 
             if (and1 == 1 && and2 == 1)
             {
-                lblSvarAnd.Text = "0";
-                lblSvarAnd.ForeColor = Color.Red;
+                lblNandSvar.Text = "0";
+                lblNandSvar.ForeColor = Color.Red;
             }
             else
             {
-                lblSvarAnd.Text = "1";
-                lblSvarAnd.ForeColor = Color.Green;
+                lblNandSvar.Text = "1";
+                lblNandSvar.ForeColor = Color.Green;
             }
         }
     }
